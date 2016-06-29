f8a933f0a717f05a469e55845a00f120858008c1
Jack McDade <jack@jackmcdade.com>
2016-06-29 15:42:55 -0400

    fix missing comma.

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'enforce_auth' => 'Enforce Authentication',
-    'enforce_auth_instruct' => "When enabled users must be logged in to use Workshop tags."
+    'enforce_auth_instruct' => "When enabled users must be logged in to use Workshop tags.",
     'whitelist' => 'Whitelist fields in fieldset',
     'whitelist_instruct' => "Allow only the fields from the fieldset to be saved, dropping everything else."
 ];
